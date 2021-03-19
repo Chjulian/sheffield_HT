85d1cda82671b73238763f16ba309c991f4f58d5
Ch Julian Villabona Arenas <cj.villabona@gmail.com>
2021-03-19 15:17:52 +0000

    calculate_number_imports.R conveted to function and integrated to wards

---
+++ src/calculate_number_imports.R
@@ -0,0 +1,48 @@
+calculate_number_imports <- function(data=mydata){
+  pacman::p_load(data.table, lubridate,
+                 dplyr, magrittr, ggplot2,
+                 tidyr)
+
+    # file_name <- "dataset_imputed_2020-12-08.csv"
+  # data <- data.table::fread(file_name)
+  # message("Using ", file_name, " as input")
+  
+  # incubation period as Weibull
+  
+  dist_setup <- function(dist_shape = NULL, dist_scale = NULL) {
+    out <- purrr::partial(pweibull,
+                          shape = dist_shape,
+                          scale = dist_scale)
+    return(out)
+  }
+  
+  incubation_fn <- dist_setup(dist_shape = 2.322737,
+                              dist_scale = 6.492272)
+  
+  # grab the inpatients only (as outpatients and staff are not subject to HOCI criteria)
+  # then: calculate prob that exposure occurred before hospital admission using pre-assigned distribution
+  inpatient_data <- mydata %>%
+    dplyr::filter(category=="inpatient") %>%
+    mutate(dateofonset_foranalysis = as_date(dateofonset_foranalysis, format = "%y-%m-%d")) %>%
+    mutate(dateofadmission = as_date(dateofadmission, format = "%y-%m-%d")) %>%
+    mutate(delay_admissiononset = as.numeric(dateofonset_foranalysis - dateofadmission))  %>%
+    mutate(prob_communityqcquired = ifelse(delay_admissiononset > 0, 
+                                           1 - incubation_fn(delay_admissiononset),
+                                           1))
+  
+  p <- mean(inpatient_data$prob_communityqcquired) 
+  N <- length(inpatient_data$prob_communityqcquired)
+  number_imports_average <- round(p * N)
+  number_imports_loCI <- qbinom(0.025,N,p)
+  number_imports_hiCI <- qbinom(0.975,N,p)
+  
+  proportion_imports_average <- number_imports_average / N
+  proportion_imports_loCI <- number_imports_loCI / N
+  proportion_imports_hiCI <- number_imports_hiCI / N
+  
+  number_imports_average <<- number_imports_average
+  
+  # save(number_imports_average, number_imports_loCI, number_imports_hiCI,
+  #      proportion_imports_average, proportion_imports_loCI, proportion_imports_hiCI,
+  #      file = paste("NumberImports_", file_name,".Rda"))
+}
